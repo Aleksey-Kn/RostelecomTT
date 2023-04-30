7f5fdc55cc01fca627d676e862398f9ea17648de
Aleksey <alleksey-Kn@yandex.ru>
2023-05-01 00:12:20 +0700

    Добавлен README.md

--- src/main/java/ru/rostelecom/test/dto/LongLinkResponse.java
+++ src/main/java/ru/rostelecom/test/dto/LongLinkResponse.java
@@ -1,4 +1,7 @@
 package ru.rostelecom.test.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "Длинная ссылка на требуемый ресурс")
 public record LongLinkResponse(String longLink) {
 }
